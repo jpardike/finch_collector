810c0dcd085121a6ab864f26db1c8272e3260742
jpardike2 <jpardike@gmail.com>
2020-10-27 09:05:28 -0700

    Adds about and all finches pages

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Finch:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+finches = [
+    Finch('Chuck', 'Brambling', 'black head in summer, and an orange breast with white belly', 2),
+    Finch('Stubbs', 'Greenfinch', 'wheezing song', 4),
+    Finch('James', 'Scottish crossbill', 'chunky, thick-set finch with a large head and substantial bill', 0)
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import finches
 
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Welcome!</h1>')
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
